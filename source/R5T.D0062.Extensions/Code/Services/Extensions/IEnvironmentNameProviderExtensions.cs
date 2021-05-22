d25d679b4a2620f393c5bb92ff52003303c18259
David Coats <david@getsafetycone.com>
2021-05-21 17:44:13 -0700

    Added the R5T.D0062.Extensions library project. Can now test for whether the environment is a specific T0019 environment.

---
+++ source/R5T.D0062.Extensions/Code/Services/Extensions/IEnvironmentNameProviderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0019;
+
+
+namespace R5T.D0062
+{
+    /// <summary>
+    /// Note, many of these extensions exist in Microsoft.Extensions.Hosting.Abstractions, Microsoft.Extensions.Hosting.HostEnvironmentEnvExtensions.
+    /// However, they are not asynchronous, and do not work on the <see cref="IEnvironmentNameProvider"/> service! (They are not *just right* for our purposes.)
+    /// </summary>
+    public static class IEnvironmentNameProviderExtensions
+    {
+        public static async Task<bool> IsEnvironment(this IEnvironmentNameProvider environmentNameProvider, string environmentName)
+        {
+            var providedEnvironmentName = await environmentNameProvider.GetEnvironmentName();
+
+            var output = providedEnvironmentName == environmentName;
+            return output;
+        }
+
+        public static Task<bool> IsDevelopment(this IEnvironmentNameProvider environmentNameProvider)
+        {
+            return environmentNameProvider.IsEnvironment(EnvironmentNames.Development);
+        }
+
+        public static Task<bool> IsProduction(this IEnvironmentNameProvider environmentNameProvider)
+        {
+            return environmentNameProvider.IsEnvironment(EnvironmentNames.Production);
+        }
+
+        public static Task<bool> IsStaging(this IEnvironmentNameProvider environmentNameProvider)
+        {
+            return environmentNameProvider.IsEnvironment(EnvironmentNames.Statging);
+        }
+    }
+}
